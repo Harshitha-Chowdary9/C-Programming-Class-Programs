8670f07e58a8b96d2322837307eedb01750a30a4
Harshitha-Chowdary9 <121248889+Harshitha-Chowdary9@users.noreply.github.com>
2023-03-30 09:02:58 +0530

    Add files via upload

---
+++ Test 2.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define N 1000
+int main() {
+    int arr[N];
+    int n,i;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    printf("Enter an array: ");
+    for (i = 0; i< n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Reversed array: ");
+    for (i = n-1; i>=0; i--){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
